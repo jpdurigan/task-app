09c06fb743bf080d68ed122d61b4c64790e14575
jp durigan <jpdurigan@gmail.com>
2023-06-01 12:16:48 -0300

    set basic new layout

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+	AccountCircle,
+	AddBox,
+	Assignment,
+	Edit,
+	EditNote,
+	Filter,
+	FilterAlt,
+	Palette,
+	Share,
+	Sort,
+} from "@mui/icons-material";
+import {
+	Avatar,
+	Box,
+	Button,
+	ButtonGroup,
+	Card,
+	CardActionArea,
+	CardActions,
+	CardContent,
+	Checkbox,
+	Chip,
+	Container,
+	Divider,
+	IconButton,
+	Popper,
+	Stack,
+	Tooltip,
+	Typography,
+} from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export const App: React.FC = () => {
+	return (
+		<>
+			<Container maxWidth="xs">
+				<Stack direction="row" alignItems="center" justifyContent="right">
+					<Assignment sx={{ fontSize: 48 }} />
+					<Typography variant="h3" component="h1">
+						Tarefas
+					</Typography>
+				</Stack>
+				<Box mb={2}>
+					<Stack direction="row" justifyContent="space-between">
+						<Stack direction="row">
+							<Tooltip title="Conta">
+								<IconButton>
+									<AccountCircle />
+								</IconButton>
+							</Tooltip>
+							<Tooltip title="Compartilhar">
+								<IconButton>
+									<Share />
+								</IconButton>
+							</Tooltip>
+						</Stack>
+						<Stack direction="row-reverse">
+							<Tooltip title="Nova tarefa">
+								<IconButton>
+									<AddBox />
+								</IconButton>
+							</Tooltip>
+							<Tooltip title="Filtrar tarefas">
+								<IconButton>
+									<FilterAlt />
+								</IconButton>
+							</Tooltip>
+              <Popper open={true}>
+                <Box>
+                  <Stack>
+                    <ButtonGroup orientation="vertical" variant="text">
+                      <Button>Botão 1</Button>
+                      <Button>Botão 2</Button>
+                    </ButtonGroup>
+                  </Stack>
+                </Box>
+              </Popper>
+							<Tooltip title="Editar tags">
+								<IconButton>
+									<Palette />
+								</IconButton>
+							</Tooltip>
+						</Stack>
+					</Stack>
+          <Divider />
+				</Box>
+				<Card>
+					<CardActionArea>
+						<CardContent>
+							<Stack direction="row" justifyContent="flex-end" gap={1}>
+								<Chip label="Tag 1" size="small" />
+								<Chip label="Tag 2" size="small" />
+							</Stack>
+							<Typography variant="h6">Descrição da tarefa</Typography>
+						</CardContent>
+					</CardActionArea>
+				</Card>
+			</Container>
+		</>
+	);
+};
