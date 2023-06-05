93d64de96a742b7899d502165a95ef435ae1c9c4
jp durigan <jpdurigan@gmail.com>
2023-06-04 21:30:30 -0300

    add manual chunks for performance

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const manualChunks = (id: string) => {
+	if (id.includes("node_modules")) {
+		if (id.includes("firebase/firestore")) {
+			return "firebase-firestore";
+		} else if (id.includes("firebase/auth")) {
+			return "firebase-auth";
+		} else if (id.includes("mui")) {
+			return "material";
+		}
+
+		return "vendor";
+	}
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: manualChunks,
+			},
+		},
+	},
+});
